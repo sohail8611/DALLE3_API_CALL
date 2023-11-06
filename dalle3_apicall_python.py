33ae848a1c90e87a73a6781d96b08b4d1d4b2a9d
sohail8611 <82253012+sohail8611@users.noreply.github.com>
2023-11-07 02:04:49 +0500

    Create dalle3_apicall_python.py

---
+++ dalle3_apicall_python.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import os
+
+# Replace with your OpenAI API key
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Define the API endpoint and request payload
+api_url = 'https://api.openai.com/v1/images/generations'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {OPENAI_API_KEY}'
+}
+data = {
+    "model": "dall-e-3",
+    "prompt": "a white siamese cat",
+    "n": 1,
+    "size": "1024x1024"
+}
+
+# Make the API request
+response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
+# Check if the request was successful
+if response.status_code == 200:
+    result = response.json()
+    print("Generated image URL:", result['data'][0]['url'])
+else:
+    print("Error:", response.status_code, response.text)
